6e0f98bd3e5814e87bbbcce151137c93fae93a80
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-08-22 08:25:47 +1030

    boolean_13.py

--- boolean_13.py
+++ boolean_13.py
@@ -2,7 +2,7 @@ def main(a, b, c):
     return (
         ((a > 0) and (b < 0) and (c < 0))
         or ((a < 0) and (b > 0) and (c < 0))
-        or ((a > 0) and (b < 0) and (c > 0))
+        or ((a < 0) and (b < 0) and (c > 0))
     )
 
 

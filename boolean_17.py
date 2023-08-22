45881487cd84d9bdaa1c13c592ddbcbaf86e388e
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-08-22 17:31:18 +1030

    boolean_17.py

--- boolean_17.py
+++ boolean_17.py
@@ -1,5 +1,5 @@
-def main(a, b, c):
-    return a > 0 and b > 0 and c > 0 and a % 2 == 1 and b % 2 == 1 and c % 2 == 1
+def main(a):
+    return 99 < a < 999 and a % 2 == 1
 
 
-print(main(int(input()), int(input()), int(input())))
+print(main(int(input())))

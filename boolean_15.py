3beea4b9942a8d856ee53172bb384b05bff1d40e
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-08-22 13:23:37 +1030

    boolean_15.py

---
+++ boolean_15.py
@@ -0,0 +1,9 @@
+def main(a, b, c):
+    return (
+        ((a > 0) and (b > 0) and (c < 0))
+        or ((a < 0) and (b > 0) and (c > 0))
+        or ((a > 0) and (b < 0) and (c > 0))
+    )
+
+
+print(main(int(input()), int(input()), int(input())))
